6da3e5751b5a698a3ba275a7f15eb169ff78579d
JanserikBolat <180103308@stu.sdu.edu.kz>
2021-08-04 16:27:13 +0600

    Изменения в роутере

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,17 @@
 import VueRouter from 'vue-router'
-import UserRegister from '../components/UserRegister.vue'
-import TimetableWrapper from '../components/TimetableWrapper.vue'
-import OrderHeader from '../components/OrderHeader.vue'
+import UserRegister from '../pages/UserRegister.vue'
+import TimetableWrapper from '../pages/TimetableWrapper.vue'
+import Order from '../pages/Order.vue'
+import SingleDay from '../pages/SingleDay.vue'
+import OrderConfirmation from '../pages//OrderConfirmation.vue'
+import OrderView from '../pages/OrderView.vue'
+import SingleOrder from '../pages/SingleOrder.vue'
+import SingleBooking from '../pages/SingleBooking.vue'
+import EditBooking from '../pages/EditBooking.vue'
+import * as dayjs from 'dayjs'
+import 'dayjs/locale/ru'
 const router =  new VueRouter({
+    // mode: 'history',
     routes: [
         {
             name: 'registerUser',
@@ -13,14 +22,50 @@ const router =  new VueRouter({
         {
             path: '/order',
             name: 'order',
-            component: OrderHeader,
+            component: Order,
             props: (route)=>({...route.params})
         },
         {
-            path: '/',
+            path: '/day/:id',
+            name: 'day',
+            component: SingleDay
+        },
+        {
+            path: '/confirmation',
+            name: 'confirmation',
+            component: OrderConfirmation,
+            props: (route)=>({...route.params})
+        },
+        {
+            path: '/allTime',
             name: 'main',
             component: TimetableWrapper
-        }
+        },
+        {
+            path: '/orders',
+            name: 'orders',
+            component: OrderView
+        },
+        {
+            path: '/order/:id',
+            name: 'singleOrder',
+            component: SingleOrder,
+        },
+        {
+            path: '/order/:id/:id',
+            name:'singleBooking',
+            component: SingleBooking
+        },
+        {
+            path: '/order/:id/:id/edit',
+            name: 'edit',
+            component: EditBooking
+        },
+        {
+            path: '/',
+            redirect: `day/${dayjs().format('DD-MM-YYYY')}`,
+        },
+    
     ]
 })
 export default router
