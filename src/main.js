5719fcbb35c6d423896976816a9a0a945a573e4e
JanserikBolat <180103308@stu.sdu.edu.kz>
2021-08-05 08:04:00 +0600

    Подключение vuex, uuid

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router  from './router/index'
+import store from './store/store'
 import VueRouter from 'vue-router'
 import Vuelidate from 'vuelidate'
+import UUID from 'vue-uuid'
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(Vuelidate)
+Vue.use(UUID)
 new Vue({
   render: h => h(App),
-  router
+  router, store: store
 }).$mount('#app')
